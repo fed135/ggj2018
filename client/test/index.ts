65f0c634510539a6d98aa7b542e9ed7e58176723
Damon Perron-Laurin <dplaurin@outbox.com>
2018-01-27 19:49:55 +0000

    Move map parsing to client/

--- client/test/index.ts
+++ client/test/index.ts
@@ -1,62 +1,7 @@
-import {uniq, keys, isEmpty} from 'lodash';
 import {it, describe} from 'mocha';
 import {expect} from 'chai';
 import mapData from '../assets/map/map_dev';
-
-type MapData = {
-  width: number,
-  tileWidth: number,
-  tileHeight: number,
-  tiles: { [key: number]: string },
-  map: number[],
-};
-
-interface Tile {
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  resourceUrl: string,
-  tileId: number,
-}
-
-type Map = Tile[];
-
-const isIn = (keys) => (key) => keys.includes(key);
-const mapValidation = (mapData: MapData): boolean => {
-  if (mapData.width <= 0) {
-    throw new Error(`Map "width" should be greater than 0. Found ${mapData.width}`);
-  }
-
-  if (isEmpty(mapData.tiles)) {
-    throw new Error('Map "tiles" should not be empty');
-  }
-  if (isEmpty(mapData.map)) {
-    throw new Error('Map "map" should not be empty');
-  }
-
-  const tileKeys = keys(mapData.tiles).map(parseInt);
-  if (!uniq(mapData.map).every(isIn(tileKeys))) {
-    throw new Error('A key was used in Map.map that was not defined in Map.tiles');
-  }
-
-  return true;
-};
-
-const parseMap = (mapData: MapData): Map => {
-  mapValidation(mapData);
-
-  return mapData.map.map((tileId, index) => {
-    return {
-      x: Math.floor(index % mapData.width) * mapData.tileWidth,
-      y: Math.floor(index / mapData.width) * mapData.tileHeight,
-      width: mapData.tileWidth,
-      height: mapData.tileHeight,
-      resourceUrl: mapData.tiles[tileId],
-      tileId,
-    };
-  });
-};
+import {Map, mapValidation, parseMap} from "../map/Map";
 
 describe('mapValidation', () => {
   it('should return true if the data is valid', () => {
