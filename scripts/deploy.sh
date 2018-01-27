477e5cfa9dbc65c893addbd7121c554d05a8009d
Frederic Charette <fredericcharette@gmail.com>
2018-01-27 08:43:02 -0500

    fixed stupid travis bug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -65,7 +65,8 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $encrypted_96214dec6a0b_key -iv $encrypted_96214dec6a0b_iv -in ggj2018_key.enc -out ggj2018_key -dchmod 600 ../deploy_key
+openssl aes-256-cbc -K $encrypted_96214dec6a0b_key -iv $encrypted_96214dec6a0b_iv -in ggj2018_key.enc -out ggj2018_key -d
+chmod 600 ../deploy_key
 eval `ssh-agent -s`
 ssh-add ../deploy_key
 
