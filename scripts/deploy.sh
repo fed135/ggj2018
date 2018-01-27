1fa6362fe266cd94932985da469dce99ee83a054
Damon Perron-Laurin <dplaurin@outbox.com>
2018-01-27 16:36:35 +0000

    Change bad config -dist

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -76,7 +76,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $encrypted_96214dec6a0b_key -iv $encrypted_96214dec6a0b_iv -in ggj2018_key.enc -dist ggj2018_key -d
+openssl aes-256-cbc -K $encrypted_96214dec6a0b_key -iv $encrypted_96214dec6a0b_iv -in ggj2018_key.enc -out ggj2018_key -d
 chmod 600 ggj2018_key
 eval `ssh-agent -s`
 ssh-add ggj2018_key
