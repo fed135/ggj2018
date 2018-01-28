d834184cc486ad9db4ef9db4257d9f8deeb926e6
Damon Perron-Laurin <dplaurin@outbox.com>
2018-01-28 18:37:17 +0000

    Change move logic location (does not belond in Avatar view code)

--- client/components/Avatar.ts
+++ client/components/Avatar.ts
@@ -1,92 +1,8 @@
-import {TimelineLite, Power0} from 'gsap';
+import {TimelineLite} from 'gsap';
 import {MapData} from "../map/Map";
+import Point = PIXI.Point;
 
-export enum Action {
-  UP='top',
-  DOWN='bottom',
-  LEFT='left',
-  RIGHT='right',
-}
 
-type Point = {
-  x: number,
-  y: number,
-}
-
-type MoveCommands = {
-  [move: string]: (
-    timeLine: TimelineLite,
-    target: PIXI.Container,
-    map: MapData,
-    lastPosition: Point,
-  ) => Point;
-}
-
-const baseMovement = {
-  ease: Power0.easeNone
-};
-
-const SPEED: number = .62;
-
-const MOVES: MoveCommands = {
-  [Action.UP]: (timeLine: TimelineLite, target: PIXI.Container, map: MapData, lastPosition: Point): Point => {
-    const newPosition = {
-      ...baseMovement,
-      ...lastPosition,
-      y: lastPosition.y -= map.tileHeight,
-    };
-
-    timeLine.to(
-      target,
-      SPEED,
-      newPosition,
-    );
-    return newPosition;
-  },
-
-  [Action.DOWN]: (timeLine: TimelineLite, target: PIXI.Container, map: MapData, lastPosition: Point): Point => {
-    const newPosition = {
-      ...baseMovement,
-      ...lastPosition,
-      y: lastPosition.y += map.tileHeight,
-    };
-
-    timeLine.to(
-      target,
-      SPEED,
-      newPosition,
-    );
-    return newPosition;
-  },
-  [Action.LEFT]: (timeLine: TimelineLite, target: PIXI.Container, map: MapData, lastPosition: Point): Point => {
-    const newPosition = {
-      ...baseMovement,
-      ...lastPosition,
-      x: lastPosition.x -= map.tileWidth,
-    };
-
-    timeLine.to(
-      target,
-      SPEED,
-      newPosition,
-    );
-    return newPosition;
-  },
-  [Action.RIGHT]: (timeLine: TimelineLite, target: PIXI.Container, map: MapData, lastPosition: Point): Point => {
-    const newPosition = {
-      ...baseMovement,
-      ...lastPosition,
-      x: lastPosition.x += map.tileWidth,
-    };
-
-    timeLine.to(
-      target,
-      SPEED,
-      newPosition,
-    );
-    return newPosition;
-  },
-};
 
 export default class Avatar extends PIXI.Sprite {
   private graphic: PIXI.Graphics = new PIXI.Graphics();
@@ -98,11 +14,4 @@ export default class Avatar extends PIXI.Sprite {
     this.addChild(this.graphic);
   }
 
-  move(moves: Action[], rawMapData: MapData) {
-    const timeLine = new TimelineLite();
-    let positionAccumulator: Point = {x: this.x, y: this.y};
-    moves.forEach((move) => {
-      positionAccumulator = MOVES[move](timeLine, this, rawMapData, positionAccumulator);
-    });
-  }
 }
--- client/components/UIWrapper.ts
+++ client/components/UIWrapper.ts
@@ -1,50 +1,50 @@
 import * as PIXI from 'pixi.js';
 import ArrowButton from './ArrowButton';
 import MoveIndicator from './MoveIndicator';
-import {Action} from "../components/Avatar";
-import { EventEmitter } from 'events';
+import {EventEmitter} from 'events';
 import config from '../config';
+import {Action} from "../Step";
 
 export default class UIWrapper {
 
-    private box = new PIXI.Graphics();
+  private box = new PIXI.Graphics();
 
-    public inputs = {
-        top: null,
-        left: null,
-        right: null,
-        bottom: null,
+  public inputs = {
+    top: null,
+    left: null,
+    right: null,
+    bottom: null,
+  };
+
+  public moves = [];
+
+  constructor(container: PIXI.Container, inputManager: EventEmitter) {
+    const uiSize = 0.16;
+
+    // Color
+    this.box.beginFill(0xDDDDDD, 0.8);
+
+    this.box.drawRect(0, 0, container.width * uiSize, container.height);
+    this.box.endFill();
+
+    this.box.width = container.width * uiSize;
+    this.box.height = container.height;
+
+    // Arrows
+    this.inputs = {
+      top: new ArrowButton(this.box, Action.UP, inputManager),
+      left: new ArrowButton(this.box, Action.LEFT, inputManager),
+      right: new ArrowButton(this.box, Action.RIGHT, inputManager),
+      bottom: new ArrowButton(this.box, Action.DOWN, inputManager)
     };
 
-    public moves = [];
-
-    constructor(container: PIXI.Container, inputManager: EventEmitter) {
-        const uiSize = 0.16;
-        
-        // Color
-        this.box.beginFill(0xDDDDDD, 0.8);
-        
-        this.box.drawRect(0,0,container.width * uiSize,container.height);
-        this.box.endFill();
-
-        this.box.width = container.width * uiSize;
-        this.box.height = container.height;
-
-        // Arrows
-        this.inputs = {
-            top: new ArrowButton(this.box, Action.UP, inputManager),
-            left: new ArrowButton(this.box, Action.LEFT, inputManager),
-            right: new ArrowButton(this.box, Action.RIGHT, inputManager),
-            bottom: new ArrowButton(this.box, Action.DOWN, inputManager)
-        };
-
-        // Move boxes
-        this.moves.length = config.playsPerTurn;
-        for (let i = 0; i<config.playsPerTurn; i++) {
-            this.moves[i] = new MoveIndicator(this.box, i, inputManager);
-        }
-
-        // Add wrapper
-        container.addChild(this.box);
+    // Move boxes
+    this.moves.length = config.playsPerTurn;
+    for (let i = 0; i < config.playsPerTurn; i++) {
+      this.moves[i] = new MoveIndicator(this.box, i, inputManager);
     }
+
+    // Add wrapper
+    container.addChild(this.box);
+  }
 }
